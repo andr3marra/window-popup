56f30412614142890954b16427e99330c81fe306
DESKTOP-X570\andre <marralopes@gmail.com>
2022-02-22 00:36:18 -0300

    Adding support to other sites than youtube

--- bg.js
+++ bg.js
@@ -1,10 +1,13 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
     const url = tab.url;
-    if(!url.match("youtube.com")){return;}
-
-    const videoId = url.match(/([A-Za-z0-9_\-]{11})/);
-    if(!videoId){return;}
-    const finalUrl = "https://www.youtube.com/embed/"+videoId[0];
+    var finalUrl = "";
+    if (url.match("youtube.com")) {                         // YOUTUBE
+        const videoId = url.match(/([A-Za-z0-9_\-]{11})/);
+        if (!videoId) { return; }
+        finalUrl = "https://www.youtube.com/embed/" + videoId[0];
+    } else {                                                // OTHERS
+        finalUrl = tab.url;
+    }
     chrome.windows.create({
         url: finalUrl,
         type: "popup"
